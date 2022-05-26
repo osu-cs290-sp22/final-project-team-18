cf4931acabb681bcb30a96db8352337719b4910a
Sabyatha Kumar <sabyathakumar@Sabyathas-MacBook-Pro.local>
2022-05-26 16:52:05 -0700

    Adding fourtune data and js

--- final/index.js
+++ final/index.js
@@ -12,3 +12,23 @@ close.onclick = function() {
     modal_drop.style.display = "none";
     instructions.style.display="none";
 }
+
+
+var btn1 = document.getElementById('button-1');
+var btn2 = document.getElementById('button-2');
+var btn3 = document.getElementById('button-3');
+var btn4 = document.getElementById('button-4');
+
+btn1.addEventListener('click', function handleClick() {
+  btn1.innerHTML = "1"
+});
+btn2.addEventListener('click', function handleClick() {
+    btn2.innerHTML = "2"
+  });
+
+  btn3.addEventListener('click', function handleClick() {
+    btn3.innerHTML = "3"
+  });
+  btn4.addEventListener('click', function handleClick() {
+    btn4.innerHTML = "4"
+  });
