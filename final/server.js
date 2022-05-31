1b1de31821d2e0776cdbeb71bf3dc8630b05b965
Carly Lane <lanecarl@oregonstate.edu>
2022-05-31 10:42:22 -0700

    fixed error in office hours

--- final/server.js
+++ final/server.js
@@ -1,12 +1,12 @@
-var path = require('path'); 
-var express = require('express'); 
-var exphbs = require('express-handlebars'); 
-var fortunes = require('./fortunes.json'); 
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var fortunes = require('./fortunes.json');
 
-var app = express(); 
-var port = process.env.PORT || 3000; 
+var app = express();
+var port = process.env.PORT || 3000;
 
-app.use(express.static('public')); 
+app.use(express.static('public'));
 
 app.engine('.handlebars', exphbs.engine({
     defaultLayout: 'main'
@@ -14,10 +14,10 @@ app.engine('.handlebars', exphbs.engine({
 
 app.set('view engine', '.handlebars');
 
-app.get('/fortune', function(req, res){
+app.get('/', function(req, res){
     res.status(200).render('findFortune')
 })
 
 app.listen(port, function(){
-    console.log("== Server is listening on port", port); 
+    console.log("== Server is listening on port", port);
 })
