5e7927b7fdcf62212dffe66db4d2534b6da1d0ea
Sabyatha Kumar <sabyathakumar@Sabyathas-MacBook-Pro.local>
2022-05-31 14:51:51 -0700

    Button js and finished css

--- final/public/index.js
+++ final/public/index.js
@@ -14,21 +14,114 @@ close.onclick = function() {
 }
 
 
+function changeBtn_value(btn,val){
+ btn.innerText=val;
+}
+
+
+function changeInstruction(){
+var instr=document.getElementsByClassName("fortune-button-box game-box");
+instr.innerText="HELLO"
+}
+var numBtclicked=0;
+var userFinalchoice=""
+
+function changeBtn(btn,clicked){
+  numBtclicked++
+  if(btn.innerHTML=="1"){
+    changeBtn_value(btn,"6")
+
+  }
+  else if(btn.innerText=="2"){
+    changeBtn_value(btn,"8")
+
+  }
+  else if(btn.innerText=="3"){
+    changeBtn_value(btn,"5")
+    
+  }
+  else if(btn.innerText=="4"){
+    changeBtn_value(btn,"7")
+    
+  }
+  else if(btn.innerText=="5"){
+    changeBtn_value(btn,"4")
+  }
+  else if(btn.innerText=="6"){
+    changeBtn_value(btn,"3")
+    
+  }
+  else if(btn.innerText=="7"){
+    changeBtn_value(btn,"2")
+    
+  }
+  else if(btn.innerText=="8"){
+    changeBtn_value(btn,"1")
+  }
+  else if(btn.innerText=="Sunshiney Blue"){
+    changeBtn_value(btn,"1")
+    
+  }
+  else if(btn.innerText=="Toxic Positivity Pink"){
+    changeBtn_value(btn,"2")
+    
+  }
+  else if(btn.innerText=="Morally Gray"){
+    changeBtn_value(btn,"3")
+    
+  }
+  else if(btn.innerText=="Indecisively(?) White"){
+    changeBtn_value(btn,"4")
+  }
+  if(numBtclicked==12){
+    userFinalchoice=clicked.innerText
+    console.log("==user string",userFinalchoice);
+  }
+}
+
+
 var btn1 = document.getElementById('button-1');
 var btn2 = document.getElementById('button-2');
 var btn3 = document.getElementById('button-3');
 var btn4 = document.getElementById('button-4');
+var onStart=document.getElementsByClassName("modal-start-button")[0];
+
+
+onStart.addEventListener('click', function handleClick() {
+  var name=document.getElementById("name-input").value;
+  console.log("==name",name);
+
+
+});
 
 btn1.addEventListener('click', function handleClick() {
-  btn1.innerHTML = "1"
+  changeBtn(btn1,btn1)
+  changeBtn(btn2,btn1)
+  changeBtn(btn3,btn1)
+  changeBtn(btn4,btn1)
 });
 btn2.addEventListener('click', function handleClick() {
-    btn2.innerHTML = "2"
+  changeBtn(btn1,btn2)
+  changeBtn(btn2,btn2)
+  changeBtn(btn3,btn2)
+  changeBtn(btn4,btn2)
   });
 
   btn3.addEventListener('click', function handleClick() {
-    btn3.innerHTML = "3"
+    changeBtn(btn3,btn3)
+    changeBtn(btn1,btn3)
+    changeBtn(btn2,btn3)
+    changeBtn(btn4,btn3)
   });
   btn4.addEventListener('click', function handleClick() {
-    btn4.innerHTML = "4"
+    changeBtn(btn4,btn4)
+    changeBtn(btn3,btn4)
+    changeBtn(btn2,btn4)
+    changeBtn(btn1,btn4)
   });
+
+
+  // var userName=""
+  // onStart.addEventListener('click', function handleClick() {
+  //   userName=document.getElementById("name-input").value;
+  // });
